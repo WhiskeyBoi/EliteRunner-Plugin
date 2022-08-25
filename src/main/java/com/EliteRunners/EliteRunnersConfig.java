2e0db324c3d1ebf170dab71280fd57a47dfeb9f1
WhiskeyBoi <91634589+WhiskeyBoi@users.noreply.github.com>
2022-08-25 01:05:08 -0400

    New functionalities added
    Screen stretching, item trading and entity hiding

--- src/main/java/com/EliteRunners/EliteRunnersConfig.java
+++ src/main/java/com/EliteRunners/EliteRunnersConfig.java
@@ -1,19 +1,120 @@
-package com.example;
+package com.EliteRunners;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
+import net.runelite.client.config.Units;
 
-@ConfigGroup("example")
-public interface ExampleConfig extends Config
+@ConfigGroup(EliteRunnersConfig.GROUP)
+public interface EliteRunnersConfig extends Config
 {
-	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+	String GROUP = "EliteRunners";
+	@ConfigSection(
+		name = "Time Trial Mode",
+		description = "This mode enables you to make an official solo run time trial",
+		position = 0
 	)
-	default String greeting()
-	{
-		return "Hello";
-	}
+	String settings = "settings";
+	////////////////////////////
+	//---------Trials---------//
+	////////////////////////////
+		@ConfigItem(
+				position = 1,
+				keyName = "DiscordName",
+				name = "Discord Tag",
+				description = "Your discord username (#xxxx included)",
+				section = settings
+		)
+		default String DiscordName() { return "EliteRunners";}
+
+		@ConfigItem(
+				position = 2,
+				keyName = "StartTrial",
+				name = "Activate Time Trial Mode",
+				description = "Check to get the time trial mode ready",
+				section = settings
+		)
+		default boolean StartTrial() { return false; }
+	/////////////////////////////////
+	//---------Runner Mode---------//
+	/////////////////////////////////
+	@ConfigSection(
+			name = "Essence Runner Mode",
+			description = "Every essence running functionalities you need",
+			position = 100
+	)
+	String EssRunMode = "EssRunMode";
+
+		@ConfigItem(
+				position = 101,
+				keyName = "Stretch",
+				name = "Screen Stretcher",
+				description = "Stretches your menu for better reach and visibility",
+				section = EssRunMode
+		)
+		default boolean Stretch() { return false; }
+
+		@ConfigItem(
+				position = 102,
+				keyName = "NoDrag",
+				name = "No Drag",
+				description = "Makes sure you don't move an Item you wish to click on",
+				section = EssRunMode
+		)
+		default boolean NoDrag() { return false; }
+
+		@ConfigItem(
+				position = 103,
+				keyName = "HideOthers",
+				name = "Hide Others",
+				description = "Hides every player not in your clan or friend list",
+				section = EssRunMode
+		)
+		default boolean HideOthers() { return false; }
+
+		@ConfigItem(
+				position = 104,
+				keyName = "Highlights",
+				name = "Enable Highlight",
+				description = "Will highlight binding necklaces, rings of dueling and trade window when needed",
+				section = EssRunMode
+		)
+		default boolean Highlights() { return false; }
+
+		@ConfigItem(
+				position = 105,
+				keyName = "ShiftClick",
+				name = "Enable Shift Click Shortcuts",
+				description = "Allows shift click shortcuts for cape, talismans, potions, necklaces, rings, essence, pouches, banks, etc.",
+				section = EssRunMode
+		)
+		default boolean ShiftClick() { return false; }
+
+		@ConfigItem(
+				position = 106,
+				keyName = "Swap",
+				name = "Enable Partial Menu Entry Swapper",
+				description = "Swaps bank deposits, ring of elements,  ring of dueling, amulet of eternal glory, and other teleportation means",
+				section = EssRunMode
+		)
+		default boolean Swap() { return false; }
+
+		@ConfigItem(
+				position = 107,
+				keyName = "OfferAll",
+				name = "Offer All Items",
+				description = "Allows you to offer every item of a kind by clicking on one of them",
+				section = EssRunMode
+		)
+		default boolean OfferAll() { return false; }
+
+		@ConfigItem(
+				position = 108,
+				keyName = "TradeSpam",
+				name = "Enable Trade Spam",
+				description = "When receiving a trade offer, it will be spammed instead",
+				section = EssRunMode
+		)
+		default boolean TradeSpam() { return false; }
 }
--- src/main/java/com/EliteRunners/EliteRunnersPlugin.java
+++ src/main/java/com/EliteRunners/EliteRunnersPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.EliteRunners;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -9,45 +9,283 @@ import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.input.MouseListener;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.events.ConfigChanged;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.api.events.ResizeableChanged;
+import net.runelite.client.input.MouseManager;
+import com.EliteRunners.EliteRunnersConfig;
+import com.EliteRunners.TranslateMouseListener;
+import com.EliteRunners.TranslateMouseWheelListener;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.inject.Provider;
+import com.google.inject.Provides;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.*;
+import net.runelite.api.events.*;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.client.callback.ClientThread;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
+import net.runelite.client.menus.MenuManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.ClientToolbar;
+import net.runelite.client.ui.NavigationButton;
+import net.runelite.client.ui.overlay.OverlayManager;
+import net.runelite.client.util.ImageUtil;
+import net.runelite.client.util.Text;
+import okhttp3.*;
+import net.runelite.api.GraphicID;
+import net.runelite.api.GraphicsObject;
+import net.runelite.api.NPC;
+import net.runelite.api.Player;
+import net.runelite.api.Projectile;
+import net.runelite.api.Renderable;
+import net.runelite.client.callback.Hooks;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
+import net.runelite.client.game.NpcUtil;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import com.google.common.annotations.VisibleForTesting;
+
+
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+		name = "EliteRunners plugin",
+		description = "Gives a multitude of options to facilitate essence and bone running",
+		tags = {"runner", "running"},
+		enabledByDefault = false
 )
-public class ExamplePlugin extends Plugin
-{
+public class EliteRunnersPlugin extends Plugin {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private EliteRunnersConfig config;
 
-	@Override
-	protected void startUp() throws Exception
-	{
-		log.info("Example started!");
+	@Inject
+	private MouseManager mouseManager;
+
+	@Inject
+	private TranslateMouseListener mouseListener;
+
+	@Inject
+	private NpcUtil npcUtil;
+
+	private boolean hideOthers;
+	private boolean hideOthers2D;
+	private boolean hideFriends;
+	private boolean hideFriendsChatMembers;
+	private boolean hideClanMembers;
+	private boolean hideNPCs;
+
+	private boolean hideLocalPlayer;
+	private boolean hideLocalPlayer2D;
+	@Inject
+	private TranslateMouseWheelListener mouseWheelListener;
+
+	@Inject
+	private ConfigManager configManager;
+
+	@Inject
+	private Hooks hooks;
+
+	public static NavigationButton panel;
+	private final ArrayListMultimap<String, Integer> indexes = ArrayListMultimap.create();
+	private final Hooks.RenderableDrawListener drawListener = this::shouldDraw;
+
+	@Provides
+	EliteRunnersConfig provideConfig(ConfigManager configManager) {
+		return configManager.getConfig(EliteRunnersConfig.class);
 	}
 
+
 	@Override
-	protected void shutDown() throws Exception
-	{
-		log.info("Example stopped!");
+	protected void startUp() {
+		mouseManager.registerMouseListener(0, (MouseListener) mouseListener);
+		mouseManager.registerMouseWheelListener(0, mouseWheelListener);
+		client.setStretchedEnabled(config.Stretch());
+		hooks.registerRenderableDrawListener(drawListener);
+
+		updateConfig();
 	}
+
+
+	@Override
+	protected void shutDown() throws Exception {
+		client.setStretchedEnabled(false);
+		client.invalidateStretching(true);
+		mouseManager.unregisterMouseListener((MouseListener) mouseListener);
+		mouseManager.unregisterMouseWheelListener(mouseWheelListener);
+		hooks.unregisterRenderableDrawListener(drawListener);
+	}
+
 
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+	public void onConfigChanged(ConfigChanged event) {
+		if (!event.getGroup().equals(EliteRunnersConfig.GROUP)) {
+			return;
+		}
+
+		updateConfig();
+	}
+
+	private void updateConfig() {
+		client.setStretchedIntegerScaling(false);
+		client.setStretchedKeepAspectRatio(true);
+		client.setStretchedFast(true);
+		client.setScalingFactor(35);
+		client.setStretchedEnabled(config.Stretch());
+		client.invalidateStretching(true);
+		hideOthers = config.HideOthers();
+		hideOthers2D = config.HideOthers();
+		hideFriends = false;
+		hideFriendsChatMembers = false;
+		hideClanMembers = false;
+		hideNPCs = false;
+		hideLocalPlayer = false;
+		hideLocalPlayer2D = false;
+
+	}
+
+	public void onClientTick(final ClientTick clientTick) {
+		if (config.OfferAll()) {
+			final MenuEntry[] menuEntries = client.getMenuEntries();
+			int index = 0;
+			indexes.clear();
+			for (MenuEntry entry : menuEntries) {
+				final String option = Text.removeTags(entry.getOption()).toLowerCase();
+				indexes.put(option, index++);
+			}
+
+			index = 0;
+			for (MenuEntry menuEntry : menuEntries) {
+				index++;
+				final String option = Text.removeTags(menuEntry.getOption()).toLowerCase();
+				final String target = Text.removeTags(menuEntry.getTarget()).toLowerCase();
+
+				if (option.equals("offer")) {
+					final int i = index(indexes, menuEntries, index, option, target);
+					final int id = index(indexes, menuEntries, i, "offer-all", target);
+
+					if (i >= 0 && id >= 0) {
+						final MenuEntry entry = menuEntries[id];
+						menuEntries[id] = menuEntries[i];
+						menuEntries[i] = entry;
+
+						client.setMenuEntries(menuEntries);
+
+						indexes.clear();
+						int idx = 0;
+						for (MenuEntry e : menuEntries) {
+							final String o = Text.removeTags(e.getOption()).toLowerCase();
+							indexes.put(o, idx++);
+						}
+					}
+				}
+			}
 		}
 	}
 
-	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	public static int index(final ArrayListMultimap<String, Integer> optionIndexes, final MenuEntry[] entries, final int limit, final String option, final String target) {
+		List<Integer> indexes = optionIndexes.get(option);
+		for (int i = indexes.size() - 1; i >= 0; --i) {
+			final int idx = indexes.get(i);
+			MenuEntry entry = entries[idx];
+			String entryTarget = Text.removeTags(entry.getTarget()).toLowerCase();
+			if (idx <= limit && entryTarget.equals(target))
+				return idx;
+		}
+		return -1;
+	}
+
+	@VisibleForTesting
+	boolean shouldDraw(Renderable renderable, boolean drawingUI)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		if (renderable instanceof Player)
+		{
+			Player player = (Player) renderable;
+			Player local = client.getLocalPlayer();
+
+			if (player.getName() == null)
+			{
+				// player.isFriend() and player.isFriendsChatMember() npe when the player has a null name
+				return true;
+			}
+			// Allow hiding local self in pvp, which is an established meta.
+			// It is more advantageous than renderself due to being able to still render local player 2d
+			if (player == local)
+			{
+				return !(drawingUI ? hideLocalPlayer2D : hideLocalPlayer);
+			}
+
+			if (player.isFriend())
+			{
+				return !hideFriends;
+			}
+			if (player.isFriendsChatMember())
+			{
+				return !hideFriendsChatMembers;
+			}
+			if (player.isClanMember())
+			{
+				return !hideClanMembers;
+			}
+
+			return !(drawingUI ? hideOthers2D : hideOthers);
+		}
+		else if (renderable instanceof NPC)
+		{
+			NPC npc = (NPC) renderable;
+
+			switch (((GraphicsObject) renderable).getId())
+			{
+				case GraphicID.MELEE_NYLO_DEATH:
+				case GraphicID.RANGE_NYLO_DEATH:
+				case GraphicID.MAGE_NYLO_DEATH:
+				case GraphicID.MELEE_NYLO_EXPLOSION:
+				case GraphicID.RANGE_NYLO_EXPLOSION:
+				case GraphicID.MAGE_NYLO_EXPLOSION:
+					return false;
+				default:
+					return true;
+			}
+		}
+
+		return true;
 	}
 }
+
+
+
+
+
+
